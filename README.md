333e64c435c1c1fb8306b6fe86195831b668be57
Rain Le <shl190002@utdallas.edu>
2021-08-20 13:26:13 -0500

    Finished!!!

--- README.md
+++ README.md
@@ -2,15 +2,39 @@
 
 ## Language, Libraries, and APIs
 This coding challenge uses the Python language, the Natural Language Toolkit library, and the typing library.
+
 ## Outside Resources Used
 I used the [typing library documentation](https://docs.python.org/3/library/typing.html), the [NLTK library documentation for wordnet](https://www.nltk.org/api/nltk.corpus.reader.html#module-nltk.corpus.reader.wordnet), the [NLTK library documentation for sentiwordnet](https://www.nltk.org/api/nltk.corpus.reader.html#module-nltk.corpus.reader.sentiwordnet), this [StackOverflow page about part-of-speech tagging for sentiwordnet](https://stackoverflow.com/questions/10223314/using-sentiwordnet-3-0), and [this free e-book on the NLTK library](https://www.nltk.org/book/).
+
 ## Approach
-For my approach to this problem, I will be using rule-based sentiment analysis.
-1. The file is parsed into two blocks of text using the fact that the blocks of text in the input file are separated by a blank line.
-2. The blocks of text are preprocessed so that it is all lowercase and all punctuation and all stop words are deleted.
+For my approach to this problem, I will be using [rule-based sentiment analysis](https://monkeylearn.com/sentiment-analysis/).
+1. The file is parsed.
+2. The block of text is preprocessed so that it is all lowercase and all punctuation and all stop words are deleted.
 3. A Token class is created that stores the word token, its part of speech, and whether or not it has been negated.
-4. Using the bag-of-words approach, the structure of the sentence is disregarded and each Token is stored with its frequency in a dictionary.
+4. Using the [bag-of-words approach](https://machinelearningmastery.com/gentle-introduction-bag-words-model/), the structure of the sentence is disregarded and each Token is stored with its frequency in a dictionary.
 5. To determine if a token is negated, the preceding word is looked at. If it is "not", "no", or "nor", then the token after it is negated.
-6. The part-of-speech is also attached to the token a built-in NLTK function.
+6. The part-of-speech is also attached to the token using a built-in NLTK function.
 7. A TokenScore class is created to store the token's positive score, negative score, and objective score.
 8. The tokens are then scored using sentiwordnet. If the token is negated, the token's positive and negative score are reversed. The token's scores are also multiplied by its frequency.
+9. The results are calculated by subtracting the overall negative score from the overall positive score and then divided by the number of tokens. Similarly, the overall objective score is divided by the number of tokens.
+10. Finally, the Block, Bag, TokenScores, and results are printed to their respective files.
+
+## Summary of Results
+The block of text is positive with a score of 0.0595 and objective with a score of 0.6534.
+
+## Analysis of Results
+For the positivity/negativity score, if the score was < -0.05 it has more negative words than positive words. Else if the score was > 0.05, it has more positive words than negative words. Else, there are roughly the same amount of positive and negative words. Since the score obtained was 0.0595 for the positivity/negativity score, there are slightly more positive words than negative words. Thus, it can be concluded that the sentiment of the text is more positive.
+For the objectivity score, if the score was < 0.45 it has more subjective words than objective words. Else if the score was > 0.55 it has more objective words than subjective words. Else, there are roughly the same amount of subjective and objective words. Since the objective score was 0.6534 for the text, there are more objective words than subjective words. Thus, it can be concluded that the text is more objective.
+The results for the positivity/negativity score made sense to me because the first half seemed more negativity with some negation thrown in the mix while the second half seemed more positive to me. Therefore, it made sense that it is only slightly more positive than negative. On the other hand, the objectivity score surprised me because the text as a whole seemed more subjective since it is a story, but the results tell us that the text has more objective words than subjective words.
+
+## Explanation for Approach
+The reason I used the rule-based approach was because there was not enough sufficient training data for the machine learning approach to work. Furthermore, I used the bag-of-words approach to the problem, because maintaining the structure of the sentence would mean more rules must be created to analyze the both the macro-structure and micro-structure of the sentence. this will craete too many rules, which will become unmanageable.
+
+## To Run Solution Program
+Run `git clone https://github.com/rain-mua-le/ACM-Research-Coding-Challenge-F21.git` in the terminal, then change directories to the created folder to run these commands:
+`
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python solution.txt
+`
--- solution.py
+++ solution.py
@@ -7,6 +7,7 @@ nltk.download("averaged_perceptron_tagger")
 nltk.download("wordnet")
 nltk.download("sentiwordnet")
 
+#This class stores a token's word, part-of-speech, and whether or not it has been negated
 class Token:
     def __init__(self, word: str, pos: str, is_negated: bool) -> None:
         self.word = word
@@ -19,6 +20,7 @@ class Token:
     def __eq__(self, o: object) -> bool:
         return (self.word, self.pos, self.is_negated) == (o.word, o.pos, o.is_negated)
 
+#This class stores a token's positive score, negative score, and objective score
 class Scores:
     def __init__(self, positive: float, negative: float, objective: float) -> None:
         self.positive = positive
@@ -27,130 +29,122 @@ class Scores:
     def __str__(self) -> str:
         return "(Positive Score: " + str(self.positive) + ", Negative Score: " + str(self.negative) + ", Objective Score: " + str(self.objective) + ")"
 
-Blocks = List[str]
-Bags = List[Dict[Token, int]]
-TokenScores = List[Dict[Token, Scores]]
+#Aliases
+Bag = Dict[Token, int]
+TokenScores = Dict[Token, Scores]
 
-def parse(filename: str, stop_words_file: str) -> Blocks:
+def parse(filename: str, stop_words_file: str) -> str:
     f = open(filename, "r")
     raw_text: str = f.read()
     f.close()
-    #Make it lowercase and remove newlines, extra spaces, and punctuations and parse text into blocks
-    l: List[str] = [str(s.strip().lower().replace("\n", " ").replace("  ", " ").replace("\'", "").replace("\"", "").replace("`", "").replace(".", "").replace("?", "").replace("!", "").replace(";", "").replace(",", "")) for s in raw_text.strip().split("\n\n")]
-    #Remove stop words from blocks
+    #Make it lowercase and remove newlines, extra spaces, and punctuations
+    s:str = raw_text.strip().lower().replace("\n", " ").replace("  ", " ").replace("\'", "").replace("\"", "").replace("`", "").replace(".", "").replace("?", "").replace("!", "").replace(";", "").replace(",", "").replace("\n\n", " ")
+    #Remove stop words
     stop = [str(s) for s in open(stop_words_file).read().strip().split("\n")]
-    li: list = []
-    for s in l:
-        res = [w for w in s.split(" ") if w not in stop]
-        dummy = " ".join(res)
-        li.append(dummy)
-    return li
+    res = [w for w in s.split(" ") if w not in stop]
+    dummy = " ".join(res)
+    return dummy
 
-def bag_of_words(block: Blocks) -> Bags:
-    l: List = []
+def bag_of_words(block: str) -> Bag:
     negate: List[str] = ["not", "no", "nor"]
-    for s in block:
-        bag: Dict = {}
-        temp = s.strip().split(" ")
-        pos = nltk.pos_tag(temp)
-        for i in range(0, len(temp)):
-            dummy: str = temp[i]
-            if dummy in negate:
-                continue
-            b: bool = False
-            if i > 0 and temp[i - 1] in negate:
-                b = True
-            token = Token(dummy, pos[i][1], b)
-            if token in bag:
-                bag[token] += 1
-            else:
-                bag[token] = 1
-        l.append(bag)
-    return l
+    bag: Dict = {}
+    #Split block of text into word tokens
+    temp = block.strip().split(" ")
+    #Tag each word with its part-of-speech
+    pos = nltk.pos_tag(temp)
+    for i in range(0, len(temp)):
+        dummy: str = temp[i]
+        #If word is a negate word, skip it 
+        if dummy in negate:
+            continue
+        b: bool = False
+        #If negate word comes before, make negate property for this word true
+        if i > 0 and temp[i - 1] in negate:
+            b = True
+        token = Token(dummy, pos[i][1], b)
+        if token in bag:
+            bag[token] += 1
+        else:
+            bag[token] = 1
+    return bag
 
-def scoring(bags: Bags) -> TokenScores:
-    l: List = []
-    for blocks in bags:
-        d: Dict = {}
-        for k, v in blocks.items():
-            tag: str = ""
-            s = None
-            scores = None
-            if k.pos.startswith("J"):
-                tag = "a"
-            elif k.pos.startswith("N"):
-                tag = "n"
-            elif k.pos.startswith("R"):
-                tag = "r"
-            elif k.pos.startswith("V"):
-                tag = "v"
-            else:
-                continue
-            synsets = wn.synsets(k.word, pos=tag)
-            if not synsets:
-                continue
-            else:
-                s = synsets[0]
-            ss = swn.senti_synset(s.name())
-            p = ss.pos_score() * v
-            n = ss.neg_score() * v
-            o = ss.obj_score() * v
-            if (k.is_negated):
-                scores = Scores(n, p, o)
-            else:
-                scores = Scores(p, n, o)
-            d[k] = scores
-        l.append(d)
-    return l
+def scoring(bags: Bag) -> TokenScores:
+    d: Dict = {}
+    for k, v in bags.items():
+        tag: str = ""
+        s = None
+        scores = None
+        #Give different tags for each corresponding part-of-speech so that it will work with sentiwordnet
+        if k.pos.startswith("J"):
+            tag = "a"
+        elif k.pos.startswith("N"):
+            tag = "n"
+        elif k.pos.startswith("R"):
+            tag = "r"
+        elif k.pos.startswith("V"):
+            tag = "v"
+        else:
+            continue
+        #Get synset of word
+        synsets = wn.synsets(k.word, pos=tag)
+        if not synsets:
+            continue
+        else:
+            s = synsets[0]
+        #Get scores and multiply by frequency
+        ss = swn.senti_synset(s.name())
+        p = ss.pos_score() * v
+        n = ss.neg_score() * v
+        o = ss.obj_score() * v
+        if (k.is_negated):
+            scores = Scores(n, p, o)
+        else:
+            scores = Scores(p, n, o)
+        d[k] = scores
+    return d
 
-def results(token_scores: TokenScores, bags: Bags) -> str:
+def results(token_scores: TokenScores, bags: Bag) -> str:
     result: str = ""
-    i = 1
-    for blocks, b in zip(token_scores, bags):
-        pos_neg_score = 0
-        obj_score = 0
-        num_of_tokens = 0
-        for v in b.values():
-            num_of_tokens += v
-        for v in blocks.values():
-            pos_neg_score += v.positive - v.negative
-            obj_score += v.objective
-        pos_neg_score /= num_of_tokens
-        obj_score /= num_of_tokens
-        if pos_neg_score < -0.05:
-            result += "Block " + str(i) + " is negative with a score of " + str(pos_neg_score) + "\n"
-        elif pos_neg_score > 0.05:
-            result += "Block " + str(i) + " is positive with a score of " + str(pos_neg_score) + "\n"
-        else:
-            result += "Block " + str(i) + " is neutral with a score of " + str(pos_neg_score) + "\n"
-        if obj_score < 0.45:
-            result += "Block " + str(i) + " is objective with a score of " + str(obj_score) + "\n"
-        elif obj_score > 0.55:
-            result += "Block " + str(i) + " is subjective with a score of " + str(obj_score) + "\n"
-        else:
-            result += "Block " + str(i) + " is neither objective nor subjective with a score of " + str(obj_score)
-        i += 1
+    pos_neg_score = 0
+    obj_score = 0
+    num_of_tokens = 0
+    for v in bags.values():
+        num_of_tokens += v
+    for v in token_scores.values():
+        pos_neg_score += v.positive - v.negative
+        obj_score += v.objective
+    pos_neg_score /= num_of_tokens
+    obj_score /= num_of_tokens
+    if pos_neg_score < -0.05:
+        result += "The block of text is negative with a score of " + str(pos_neg_score) + "\n"
+    elif pos_neg_score > 0.05:
+        result += "The block of text is positive with a score of " + str(pos_neg_score) + "\n"
+    else:
+        result += "The block of text is neutral with a score of " + str(pos_neg_score) + "\n"
+    if obj_score < 0.45:
+        result += "The block of text is subjective with a score of " + str(obj_score) + "\n"
+    elif obj_score > 0.55:
+        result += "The block of text is objective with a score of " + str(obj_score) + "\n"
+    else:
+        result += "The block of text is neither objective nor subjective with a score of " + str(obj_score)
     return result
 
 if __name__ == "__main__":
-    blocks = parse("input.txt", "stop_words.txt")
+    block = parse("input.txt", "stop_words.txt")
     file_blocks = open("Blocks.txt", "w")
-    for block in blocks:
-        file_blocks.write(block + "\n")
+    file_blocks.write(block + "\n")
     file_blocks.close()
-    bags = bag_of_words(blocks)
+    bag = bag_of_words(block)
     file_bags = open("Bags.txt", "w")
-    for block in bags:
-        for k, v in block.items():
-            file_bags.write(str(k) + "- " + str(v) + "\n")
+    for k, v in bag.items():
+        file_bags.write(str(k) + "- " + str(v) + "\n")
     file_bags.close()
-    token_scores = scoring(bags)
+    token_scores = scoring(bag)
     file_token_scores = open("TokenScores.txt", "w")
-    for block in token_scores:
-        for k, v in block.items():
-            file_token_scores.write(str(k) + "- " + str(v) + "\n")
+    for k, v in token_scores.items():
+        file_token_scores.write(str(k) + "- " + str(v) + "\n")
     file_token_scores.close()
-    result = results(token_scores, bags)
+    result = results(token_scores, bag)
     file_results = open("Results.txt", "w")
     file_results.write(result)
     file_results.close()
